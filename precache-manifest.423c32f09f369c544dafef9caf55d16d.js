0f19acdb6e898bf4d50b31b8eb661b138460eefb
spencer <spencer.a.brett@gmail.com>
2019-02-26 04:13:42 -0800

    Updates

---
+++ precache-manifest.423c32f09f369c544dafef9caf55d16d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b32e004e71be4d971ba5",
+    "url": "/tictactoe/static/css/main.1db5fedb.chunk.css"
+  },
+  {
+    "revision": "b32e004e71be4d971ba5",
+    "url": "/tictactoe/static/js/main.b32e004e.chunk.js"
+  },
+  {
+    "revision": "3ce45484ea8551061fea",
+    "url": "/tictactoe/static/js/runtime~main.3ce45484.js"
+  },
+  {
+    "revision": "5d9b0fda7f2906f7739e",
+    "url": "/tictactoe/static/js/2.5d9b0fda.chunk.js"
+  },
+  {
+    "revision": "25fe11079868a6e30f5d507bedcd1474",
+    "url": "/tictactoe/index.html"
+  }
+];
